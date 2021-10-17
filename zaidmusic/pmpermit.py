856420707c5dba271a7b7a6d153cd4a6ccf7961c
Darshan <92586012+XDacchuX@users.noreply.github.com>
2021-10-17 15:16:11 +0530

    Update pmpermit.py

--- zaidmusic/pmpermit.py
+++ zaidmusic/pmpermit.py
@@ -50,7 +50,7 @@ async def autopmPermiat(client: USER, message: Message):
     chat_id = message.chat.id
     if not chat_id in pchats:
         pchats.append(chat_id)
-        await message.reply_text("approved to pm due to outgoing messages")
+        await message.reply_text("Approved PM By Dacchu")
         return
     message.continue_propagation()
 
